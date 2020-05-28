741d4aee7b4c1420dd2c37a2690792b9d7b959a8
chidvilaskoppala-hexaware <KoppalaC@hexaware.com>
2020-05-28 16:02:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ restService.use(
 
 restService.use(bodyParser.json());
 
-restService.post("/echo", function(req, res) {
+restService.post("/ssml", function(req, res) {
   var speech =
     req.body.queryResult &&
     req.body.queryResult.parameters &&
